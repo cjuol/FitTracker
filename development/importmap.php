bd8232d8087491ca90ba710262e604c3448e1250
Cristobal Jurado Oller <c.jurado.oller@gmail.com>
2026-02-18 13:39:35 +0100

    Remove frontend components and related configurations for a complete overhaul

--- development/importmap.php
+++ development/importmap.php
@@ -11,18 +11,4 @@
  *
  * The "importmap:require" command can be used to add new entries to this file.
  */
-return [
-    'app' => [
-        'path' => './assets/app.js',
-        'entrypoint' => true,
-    ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
-    ],
-    'vue/dist/vue.esm-browser.js' => [
-        'version' => '3.5.28',
-    ],
-];
+return [];
